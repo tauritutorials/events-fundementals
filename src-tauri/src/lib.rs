5c076f8e7bc5692e8012281326d61e1762698bb3
Christian <christianpavilonis@outlook.com>
2025-01-04 20:12:56 -0600

    checkpoint

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,22 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::{thread::sleep, time::Duration};
+use tauri::{AppHandle, Emitter};
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn start_process(app: AppHandle) {
+    tauri::async_runtime::spawn(async move {
+        for i in 0..=100 {
+            app.emit("progress", i).ok();
+
+            sleep(Duration::from_millis(50));
+        }
+    });
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![start_process])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,26 @@
 import { invoke } from "@tauri-apps/api/core";
-
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
+import { listen } from "@tauri-apps/api/event";
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+    let headerEl = document.querySelector("#progress-header");
+    let progressEl = document.querySelector("#progress");
+    let buttonEl: HTMLButtonElement|null = document.querySelector("#start");
+
+    if (headerEl && progressEl && buttonEl) {
+        buttonEl.addEventListener("click", async () => {
+            await invoke("start_process");
+
+            headerEl.innerHTML = "Processing...";
+            buttonEl.disabled = true;
+        });
+
+        listen("progress", (event) => {
+            progressEl.innerHTML = `${event.payload}%`;
+
+            if (event.payload === 100) {
+                headerEl.innerHTML = "Done!";
+                buttonEl.disabled = false;
+            }
+        });
+    }
 });
